19f262338ccb0a95b69eb4225f8264c64a66a143
ItoKona <lupinmine.1217@gmail.com>
2024-01-08 10:43:31 +0900

    add scaffold

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # README
 
-#Ruby on Rails Tutorials
+# Ruby on Rails Tutorials
 
 This README would normally document whatever steps are necessary to get the
 application up and running.
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root "users#index"
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-    root "application#hello"
+   
   # Defines the root path route ("/")
   # root "articles#index"
 end
